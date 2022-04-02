eed9b06f56fdf37ca5cedb9b597ce251eaa7439b
kundan mishra <mishrakundan073@gmail.com>
2022-04-02 12:05:30 +0530

    domains are now NFTs

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,25 @@
 const hre = require("hardhat");
 
 async function main() {
-  const Domains = await hre.ethers.getContractFactory("Domains");
-  const domains = await Domains.deploy();
+  const [owner, randomPerson] = await hre.ethers.getSigners();
+  const domainContractFactory = await hre.ethers.getContractFactory("Domains");
+  const domainContract = await domainContractFactory.deploy(".alps");
+  await domainContract.deployed();
+  console.log("Contract deployed to:", domainContract.address);
+  console.log("Contract deployed by:", owner.address);
 
-  await domains.deployed();
+  const txn1 = await domainContract.registerForDomain("dooms", {
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
+  await txn1.wait();
 
-  console.log("Domains deployed to:", domains.address);
+  const domainOwner = await domainContract.getDomainOwner("dooms");
+  console.log("Owner of domain:", domainOwner);
+
+  // const txn2 = await domainContract
+  //   .connect(randomPerson)
+  //   .setDomainRecord("spidey", "I am spiderman");
+  // await txn2.wait();
 }
 
 main()
